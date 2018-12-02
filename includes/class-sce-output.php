f1015e7190950074c3e804bded0208f0434846cc
Ronald Huereca <rhuereca@updraftplus.com>
2018-12-02 12:06:13 -0600

    Adding filters for modifying output.

--- includes/class-sce-output.php
+++ includes/class-sce-output.php
@@ -40,6 +40,13 @@ class SCE_Output {
 		add_filter( 'sce_text_cancel', array( $this, 'save_cancel_text' ) );
 		add_filter( 'sce_text_delete', array( $this, 'save_delete_text' ) );
 		add_filter( 'sce_text_edit', array( $this, 'edit_text' ) );
+		add_filter( 'sce_allow_delete_confirmation', array( $this, 'allow_delete_confirmation' ) );
+		add_filter( 'sce_allow_delete', array( $this, 'allow_deletion' ) );
+		add_filter( 'sce_loading_img', array( $this, 'loading_img' ) );
+		add_filter( 'sce_confirm_delete', array( $this, 'message_confirm_delete' ) );
+		add_filter( 'sce_comment_deleted', array( $this, 'message_comment_deleted' ) );
+		add_filter( 'sce_comment_deleted_error', array( $this, 'message_comment_deleted_error' ) );
+		add_filter( 'sce_empty_comment', array( $this, 'message_empty_comment' ) );
 	}
 
 	/**
@@ -52,6 +59,114 @@ class SCE_Output {
 	 */
 	private function init_actions() {
 
+	}
+
+	/**
+	 * Returns an error when a comment is empty.
+	 *
+	 * Returns an error when a comment is empty.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * 
+	 * @param string $message Empty comment error
+	 * @return string New empty comment error
+	 */
+	public function message_empty_comment( $message ) {
+		return isset( $this->options['comment_empty_error'] ) ? $this->options['comment_empty_error'] : $message;
+	}
+
+	/**
+	 * Returns a delete error.
+	 *
+	 * Returns a delete error.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * 
+	 * @param string $message Delete error message
+	 * @return string New delete error message
+	 */
+	public function message_comment_deleted_error( $message ) {
+		return isset( $this->options['comment_deleted_error'] ) ? $this->options['comment_deleted_error'] : $message;
+	}
+
+	/**
+	 * Returns a delete confirmation when a comment is removed.
+	 *
+	 * Returns a delete confirmation when a comment is removed.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * 
+	 * @param string $message Delete removal message
+	 * @return string New delete removal message
+	 */
+	public function message_comment_deleted( $message ) {
+		return isset( $this->options['comment_deleted'] ) ? $this->options['comment_deleted'] : $message;
+	}
+
+	/**
+	 * Returns a delete confirmation modal text.
+	 *
+	 * Returns a delete confirmation modal text.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * 
+	 * @param string $message Delete confirmation message
+	 * @return string New delete confirmation message
+	 */
+	public function message_confirm_delete( $message ) {
+		return isset( $this->options['confirm_delete'] ) ? $this->options['confirm_delete'] : $message;
+	}
+
+	/**
+	 * Returns a new loading image when saving a comment.
+	 *
+	 * Returns a new loading image when saving a comment.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * 
+	 * @param string $loading_image_url Loading image url
+	 * @return string New loading image url
+	 */
+	public function loading_img( $loading_image_url ) {
+		$loading_image = isset( $this->options['loading_image'] ) ? $this->options['loading_image'] : $loading_image_url;
+		return $loading_image;
+	}
+
+	/**
+	 * Returns whether a delete option is available.
+	 *
+	 * Returns whether a delete option is available.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * 
+	 * @param bool $allow_deletion Whether to allow comment deletion
+	 * @return bool Whether to allow comment deletion
+	 */
+	public function allow_deletion( $allow_deletion ) {
+		$allow_deletion = isset( $this->options['allow_delete'] ) ? $this->options['allow_delete'] : $allow_deletion;
+		return $allow_deletion;
+	}
+
+	/**
+	 * Returns whether a delete confirmation modal appears when deleting a comment.
+	 *
+	 * Returns whether a delete confirmation modal appears when deleting a comment.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * 
+	 * @param bool $allow_deletion_confirmation Whether to allow confirmation modal
+	 * @return bool Whether to allow confirmation modal
+	 */
+	public function allow_delete_confirmation( $allow_delete_confirmation ) {
+		$allow_confirmation = isset( $this->options['allow_delete_confirmation'] ) ? $this->options['allow_delete_confirmation'] : $allow_delete_confirmation;
+		return $allow_confirmation;
 	}
 
 	/**
