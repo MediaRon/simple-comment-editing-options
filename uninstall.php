7009226a62825ce43768c6259871409520c74d5d
Ronald Huereca <rhuereca@updraftplus.com>
2018-12-04 15:12:45 -0600

    Adding uninstall option.

---
+++ uninstall.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Uninstall script
+ *
+ * Uninstall script for Easy Updates Manager.
+ *
+ * @package WordPress
+ * @since 5.0.0
+ */
+
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+	exit();
+}
+delete_site_option( 'sce_options' );
+delete_site_option( 'sce_license_status' );
+delete_site_option( 'sce_table_version' );
+
+// For table removal
+global $wpdb;
+$tablename = $wpdb->base_prefix . 'sce_comments';
+$sql = "drop table if exists $tablename";
+$wpdb->query($sql);
