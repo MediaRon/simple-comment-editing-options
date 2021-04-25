4882c24045b376f272e7ed303fa76f606f498939
Ronald Huereca <ronalfy@gmail.com>
2021-04-25 12:14:47 -0500

    Add basic webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+
+module.exports = [
+	{
+		mode: process.env.NODE_ENV,
+		entry: {
+			themes: ["./css/themes.scss"],
+			"sce-cc-progress-bar": ["./css/sce-ccc-progress-bar.scss"],
+		},
+		module: {
+			rules: [
+				{
+					test: /\.scss$/,
+					exclude: /(node_modules|bower_components)/,
+					use: [
+						{
+							loader: MiniCssExtractPlugin.loader,
+						},
+						{
+							loader: "css-loader",
+							options: {
+								sourceMap: true,
+								url: false,
+							},
+						},
+						"sass-loader",
+					],
+				},
+			],
+		},
+		plugins: [
+			new FixStyleOnlyEntriesPlugin(),
+			new MiniCssExtractPlugin(),
+		],
+	},
+];
